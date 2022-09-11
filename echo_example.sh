edf2ffec8791e729b5866867ad2483869932a8cc
Ranit Debnath Akash <ranitid12@gmail.com>
2022-09-11 13:55:09 -0600

    comment and echo examples

---
+++ echo_example.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#!/bin/bash
+echo "Printing text with newline"
+echo -n "Printing text without newline"
+echo -e "\nRemoving \t backslash \t characters\n"
+echo "\nRemoving \t backslash \t characters\n"
